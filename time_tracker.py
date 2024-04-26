36137a6dc23876d40c47bc30872ac5014263fb3b
Crenshaw <crenshawdevcontact@gmail.com>
2024-04-26 09:24:19 +0200

    Implement playSound

--- time_tracker.py
+++ time_tracker.py
@@ -1,6 +1,7 @@
 from threading import Thread
 import time
 import datetime
+import winsound
 
 
 def format_timer(timer):
@@ -11,7 +12,6 @@ class TimeTracker:
     TIMER_DURATION = 1260
 
     def __init__(self):
-        print('initializing time tracker')
         self.xd_one_timer = self.TIMER_DURATION
         self.x_one_timer = self.TIMER_DURATION
         self.xd_two_timer = self.TIMER_DURATION
@@ -23,7 +23,6 @@ class TimeTracker:
     def start_xd_one_timer(self, timer):
         timer.set(format_timer(self.TIMER_DURATION))
         self.xd_one_timer = self.TIMER_DURATION
-        print('starting xd one timer')
         if not self.xd_one_timer_thread or not self.xd_one_timer_thread.is_alive():
             self.xd_one_timer_thread = Thread(target=self.run_xd_one_timer, args=(timer,), daemon=True)
             self.xd_one_timer_thread.start()
@@ -47,16 +46,18 @@ class TimeTracker:
             time.sleep(1)
             self.xd_one_timer = self.xd_one_timer - 1
             timer.set(format_timer(self.xd_one_timer))
-            print(f'xd one timer: {self.xd_one_timer}')
+        winsound.PlaySound('SystemHand', winsound.SND_ALIAS)
 
     def run_x_one_timer(self, timer):
         while self.x_one_timer > 0:
             time.sleep(1)
             self.x_one_timer = self.x_one_timer - 1
             timer.set(format_timer(self.x_one_timer))
+        winsound.PlaySound('SystemHand', winsound.SND_ALIAS)
 
     def run_xd_two_timer(self, timer):
         while self.xd_two_timer > 0:
             time.sleep(1)
             self.xd_two_timer = self.xd_two_timer - 1
             timer.set(format_timer(self.xd_two_timer))
+        winsound.PlaySound('SystemHand', winsound.SND_ALIAS)
